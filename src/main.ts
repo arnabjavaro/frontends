9b0ec322dd2137703f6e72237ed41c4c7b465dca
arnabjavaro <roy.arnab0594@gmail.com>
2024-10-08 15:46:51 +0530

    Initial commit with local files

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import {bootstrapApplication} from '@angular/platform-browser'
+import {provideRouter} from '@angular/router'
+import {AppComponent} from './app/app.component'
+import {appRoutes} from './app/app.routes'
+import {provideState, provideStore} from '@ngrx/store'
+import {provideStoreDevtools} from '@ngrx/store-devtools'
+import {isDevMode} from '@angular/core'
+import { authFeatureKey, authReducer } from './app/auth/store/reducers'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideEffects } from '@ngrx/effects'
+import * as authEffects from './app/auth/store/effects'
+import { provideRouterStore, routerReducer } from '@ngrx/router-store'
+import { authInterceptor } from './app/shared/services/authInterceptor'
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(appRoutes),
+    provideStore({
+      router: routerReducer
+    }),
+    provideRouterStore(),
+    provideState(authFeatureKey,authReducer),
+    provideEffects(authEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
+})
